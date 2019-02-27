26d38e37f2f8500042647c504f5469c37caaa583
Dongchun Zhu <dongchun.zhu@mediatek.com>
2019-02-28 01:32:10 +0800

    [ALPS04076102] Flashlight: tb8321p2_bsp: add control for Strobe/Flashlight
    [Detail]
    add main flashlight support
    
    MTK-Commit-Id: fa50cd581768291af254b219a6803d53976e14d1
    
    Change-Id: I73de74940596468ea84f90e55d4fe35bf861a971
    CR-Id: ALPS04076102
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Dongchun Zhu <dongchun.zhu@mediatek.com>

--- drivers/misc/mediatek/flashlight/flashlight-device.c
+++ drivers/misc/mediatek/flashlight/flashlight-device.c
@@ -13,11 +13,34 @@
 
 #include "flashlight-core.h"
 
-#if defined(mt6739)
-const struct flashlight_device_id flashlight_id[] = {
-	/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
-	{0, 0, 0, "flashlights-rt4505", 0, 0},
-};
+#if defined(mt6580)
+	#if defined(tb8321p2_bsp) || defined(tb8321p2_bsp_1g) || \
+	defined(tb8321p2_bsp_3gds) || defined(tb8321p2_bsp_512m) || \
+	defined(tb8321p2_bsp_trusty) || defined(tb8321p2_bspab) || \
+	defined(tb8321p2_bspota)
+	const struct flashlight_device_id flashlight_id[] = {
+		/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
+		{0, 0, 0, "flashlights_cp2155", 0, 0},
+	};
+	#else
+	const struct flashlight_device_id flashlight_id[] = {
+		/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
+		{0, 0, 0, "flashlights-lm3642", 0, 0},
+	};
+	#endif
+#elif defined(mt6739)
+	#if defined(tb8765ap1_64_bsp)
+	const struct flashlight_device_id flashlight_id[] = {
+		/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
+		{0, 0, 0, "flashlights_led191", 0, 0},
+		{1, 0, 0, "flashlights_led191", 1, 0},
+	};
+	#else
+	const struct flashlight_device_id flashlight_id[] = {
+		/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
+		{0, 0, 0, "flashlights-rt4505", 0, 0},
+	};
+	#endif
 #elif defined(mt6757)
 	#if defined(evb6757p_dm_64) || defined(k57pv1_dm_64) || \
 	defined(k57pv1_64_baymo) || defined(k57pv1_dm_64_bif) || \
@@ -42,46 +65,46 @@ const struct flashlight_device_id flashlight_id[] = {
 	};
 	#endif
 #elif defined(mt6758)
-const struct flashlight_device_id flashlight_id[] = {
-	/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
-	{0, 0, 0, "flashlights-mt6370", 0, 0},
-	{0, 1, 0, "flashlights-mt6370", 1, 0},
-};
+	const struct flashlight_device_id flashlight_id[] = {
+		/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
+		{0, 0, 0, "flashlights-mt6370", 0, 0},
+		{0, 1, 0, "flashlights-mt6370", 1, 0},
+	};
 #elif defined(mt6759)
-const struct flashlight_device_id flashlight_id[] = {
-	/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
-	{0, 0, 0, "flashlights-rt5081", 0, 0},
-	{0, 1, 0, "flashlights-rt5081", 1, 0},
-};
+	const struct flashlight_device_id flashlight_id[] = {
+		/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
+		{0, 0, 0, "flashlights-rt5081", 0, 0},
+		{0, 1, 0, "flashlights-rt5081", 1, 0},
+	};
 #elif defined(mt6763)
-const struct flashlight_device_id flashlight_id[] = {
-	/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
-	{0, 0, 0, "flashlights-mt6370", 0, 0},
-	{0, 1, 0, "flashlights-mt6370", 1, 0},
-};
+	const struct flashlight_device_id flashlight_id[] = {
+		/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
+		{0, 0, 0, "flashlights-mt6370", 0, 0},
+		{0, 1, 0, "flashlights-mt6370", 1, 0},
+	};
 #elif defined(mt6799)
-const struct flashlight_device_id flashlight_id[] = {
-	/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
-	{0, 0, 0, "flashlights-mt6336", 0, 0},
-	{0, 1, 0, "flashlights-mt6336", 1, 0},
-};
+	const struct flashlight_device_id flashlight_id[] = {
+		/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
+		{0, 0, 0, "flashlights-mt6336", 0, 0},
+		{0, 1, 0, "flashlights-mt6336", 1, 0},
+	};
 #elif defined(mt8167)
-const struct flashlight_device_id flashlight_id[] = {
-	/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
-	{0, 0, 0, "flashlights-lm3642", 0, 0},
-};
+	const struct flashlight_device_id flashlight_id[] = {
+		/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
+		{0, 0, 0, "flashlights-lm3642", 0, 0},
+	};
 #else
-const struct flashlight_device_id flashlight_id[] = {
-	/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
-	{0, 0, 0, "flashlights-none", -1, 0},
-	{0, 1, 0, "flashlights-none", -1, 0},
-	{1, 0, 0, "flashlights-none", -1, 0},
-	{1, 1, 0, "flashlights-none", -1, 0},
-	{0, 0, 1, "flashlights-none", -1, 0},
-	{0, 1, 1, "flashlights-none", -1, 0},
-	{1, 0, 1, "flashlights-none", -1, 0},
-	{1, 1, 1, "flashlights-none", -1, 0},
-};
+	const struct flashlight_device_id flashlight_id[] = {
+		/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
+		{0, 0, 0, "flashlights-none", -1, 0},
+		{0, 1, 0, "flashlights-none", -1, 0},
+		{1, 0, 0, "flashlights-none", -1, 0},
+		{1, 1, 0, "flashlights-none", -1, 0},
+		{0, 0, 1, "flashlights-none", -1, 0},
+		{0, 1, 1, "flashlights-none", -1, 0},
+		{1, 0, 1, "flashlights-none", -1, 0},
+		{1, 1, 1, "flashlights-none", -1, 0},
+	};
 #endif
 
 const int flashlight_device_num =
