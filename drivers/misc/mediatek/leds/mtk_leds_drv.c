ce4768ce44f6424816922f64e82abbf176dfcab1
Chang-An Chen <chang-an.chen@mediatek.com>
2019-02-28 01:23:50 +0800

    [ALPS03869354] leds: fix build error due to function not found
    [Detail]
    Using weak function to get rid of build error when other
    dependancy module not ready.
    
    MTK-Commit-Id: 40ced4c83076f20329ed430e76b9749867693d53
    
    Change-Id: If7149171d6df6a3a0687d75c2bb555e88c97ec3f
    Signed-off-by: Chang-An Chen <chang-an.chen@mediatek.com>
    CR-Id: ALPS03869354
    Feature: [Android Default] Backlight

--- drivers/misc/mediatek/leds/mtk_leds_drv.c
+++ drivers/misc/mediatek/leds/mtk_leds_drv.c
@@ -77,6 +77,12 @@ static int debug_enable_led = 1;
 #define LED_INTERNAL_LEVEL_BIT_CNT 10
 #endif
 
+void __weak disp_aal_notify_backlight_changed(int bl_1024) {};
+bool __weak disp_aal_is_support(void) { return false; };
+int __weak disp_bls_set_max_backlight(unsigned int level_1024) { return 0; };
+int __weak disp_bls_set_backlight(int level_1024) { return 0; }
+int __weak mtkfb_set_backlight_level(unsigned int level) { return 0; };
+void __weak disp_pq_notify_backlight_changed(int bl_1024) {};
 static int mt65xx_led_set_cust(struct cust_mt65xx_led *cust, int level);
 
 /****************************************************************************
