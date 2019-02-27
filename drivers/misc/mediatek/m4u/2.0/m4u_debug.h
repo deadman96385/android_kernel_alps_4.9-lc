82d9de6a310ac2bb81b4722676ec9f1351dee3e9
Guangming Cao <guangming.cao@mediatek.com>
2019-02-28 01:33:34 +0800

    [ALPS04230396] ion: fixed build error
    [Detail]
    remove ddp_mem_test() and __ddp_mem_test()
    
    MTK-Commit-Id: b267d87a0060f8da2824a3b82a4ac985dedd207d
    
    Change-Id: I5510947e333075bea8026019880f641a2c8288ba
    Signed-off-by: Guangming Cao <guangming.cao@mediatek.com>
    CR-Id: ALPS04230396
    Feature: [Module]Kernel Maintenance

--- drivers/misc/mediatek/m4u/2.0/m4u_debug.h
+++ drivers/misc/mediatek/m4u/2.0/m4u_debug.h
@@ -15,12 +15,15 @@
 #define __M4U_DEBUG_H__
 
 extern unsigned long gM4U_ProtectVA;
-
+#if 0
 extern int __attribute__((weak)) ddp_mem_test(void);
 extern int __attribute__((weak)) __ddp_mem_test(unsigned int *pSrc, unsigned int pSrcPa,
 			    unsigned int *pDst, unsigned int pDstPa,
 			    int need_sync);
-
+#else
+#define ddp_mem_test(...)
+#define __ddp_mem_test(...)
+#endif
 #ifdef M4U_TEE_SERVICE_ENABLE
 extern int m4u_sec_init(void);
 extern int m4u_config_port_tee(M4U_PORT_STRUCT *pM4uPort);
