74149676f5e4d29e24879d2c9d4742b1fb9a3ea0
Gray Jia <gray.jia@mediatek.com>
2019-02-28 01:34:00 +0800

    [ALPS04286459] msdc:porting 2+ to kernel-4.9-lc
    [Details]
     1.driver is ready
     2.autok is disable need fix
    
    MTK-Commit-Id: 5921b4383e998657c98342f5a25217b8e8197471
    
    Change-Id: I89e308a12f0b336f157484111f5c354d1d362639
    Signed-off-by: Gray Jia <gray.jia@mediatek.com>
    CR-Id: ALPS04286459
    Feature: eMMC Boot Up

--- include/dt-bindings/mmc/mt6735-msdc.h
+++ include/dt-bindings/mmc/mt6735-msdc.h
@@ -12,8 +12,8 @@
  * GNU General Public License for more details.
  */
 
-#ifndef _DT_BINDINGS_MMC_MT6580_H
-#define _DT_BINDINGS_MMC_MT6580_H
+#ifndef _DT_BINDINGS_MMC_MT6535_H
+#define _DT_BINDINGS_MMC_MT6535_H
 
 #define MSDC_EMMC               (0)
 #define MSDC_SD                 (1)
@@ -24,21 +24,28 @@
 
 /* each PLL have different gears for select
  * software can used mux interface from clock management module to select */
-#define MSDC0_CLKSRC_125MHZ     (0)
-#define MSDC0_CLKSRC_150MHZ     (1)
-#define MSDC0_CLKSRC_187MHZ     (2)
-#define MSDC0_CLKSRC_178MHZ     (3)
-#define MSDC0_CLKSRC_214MHZ     (4)
-#define MSDC0_CLKSRC_26MHZ      (6)
-#define MSDC0_CLKSRC_208MHZ     (7)
 
-#define MSDC1_CLKSRC_125MHZ     (0)
-#define MSDC1_CLKSRC_150MHZ     (1)
-#define MSDC1_CLKSRC_187MHZ     (2)
-#define MSDC1_CLKSRC_178MHZ     (3)
-#define MSDC1_CLKSRC_214MHZ     (4)
-#define MSDC1_CLKSRC_26MHZ      (6)
-#define MSDC1_CLKSRC_208MHZ     (7)
+#define MSDC0_CLKSRC_26MHZ		(0)
+#define MSDC0_CLKSRC_800MHZ		(1)
+#define MSDC0_CLKSRC_400MHZ		(2)
+#define MSDC0_CLKSRC_200MHZ		(3)
+#define MSDC0_CLKSRC_182MHZ		(4)
+#define MSDC0_CLKSRC_136MHZ		(5)
+#define MSDC0_CLKSRC_156MHZ		(6)
+#define MSDC0_CLKSRC_416MHZ		(7)
+#define MSDC0_CLKSRC_48MHZ		(8)
+#define MSDC0_CLKSRC_91MHZ		(9)
+#define MSDC0_CLKSRC_624MHZ		(10)
+
+#define MSDC1_CLKSRC_26MHZ		(0)
+#define MSDC1_CLKSRC_208MHZ		(1)
+#define MSDC1_CLKSRC_200MHZ		(2)
+#define MSDC1_CLKSRC_182MHZ		(3)
+#define MSDC1_CLKSRC_136MHZ		(4)
+#define MSDC1_CLKSRC_156MHZ		(5)
+#define MSDC1_CLKSRC_48MHZ		(6)
+#define MSDC1_CLKSRC_91MHZ		(7)
+
 
 #define MSDC_SMPL_RISING        (0)
 #define MSDC_SMPL_FALLING       (1)
