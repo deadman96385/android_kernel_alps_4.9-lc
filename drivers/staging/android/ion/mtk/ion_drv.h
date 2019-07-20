781dc853d7067cd5a9c875bff9cc271582569736
Guangming Cao <guangming.cao@mediatek.com>
2019-07-20 10:52:46 +0800

    [ALPS04681802] ion: update ion struct
    update ion struct
    
    MTK-Commit-Id: 93cfffc27035f6f2830fe9c934ec5a8b527be8a4
    
    Change-Id: Ie304f119df976b20853dc120faa234b1baf2a2a2
    Signed-off-by: Guangming Cao <guangming.cao@mediatek.com>
    CR-Id: ALPS04681802
    Feature: [Module]ION/M4U

--- drivers/staging/android/ion/mtk/ion_drv.h
+++ drivers/staging/android/ion/mtk/ion_drv.h
@@ -195,12 +195,27 @@ struct ion_mm_pool_info {
 	unsigned int ret;
 };
 
+struct ion_mm_get_iova_param {
+	union {
+		ion_user_handle_t handle;
+		struct ion_handle *kernel_handle;
+	};
+	int module_id;
+	unsigned int security;
+	unsigned int coherent;
+	unsigned int reserve_iova_start;
+	unsigned int reserve_iova_end;
+	u64 phy_addr;
+	unsigned long len;
+};
+
 struct ion_mm_data {
 	enum ION_MM_CMDS mm_cmd;
 	union {
 		struct ion_mm_config_buffer_param config_buffer_param;
 		struct ion_mm_buf_debug_info buf_debug_info_param;
 		struct ion_mm_pool_info pool_info_param;
+		struct ion_mm_get_iova_param get_phys_param;
 	};
 };
 
