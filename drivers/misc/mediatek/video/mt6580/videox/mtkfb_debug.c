9dd70bccdeeba1fc4bb223640b22468265783be0
Shaohua Deng <shaohua.deng@mediatek.com>
2019-09-27 11:57:42 +0800

    [ALPS04751110] DISP: disable debug log
    [Detail]
    disable debug log
    
    MTK-Commit-Id: 10d9a954ac56f003ebc19a1567851f84762075c3
    
    Change-Id: I848c13c4772240b46d129815d7d241e6203d67d1
    Signed-off-by: Shaohua Deng <shaohua.deng@mediatek.com>
    CR-Id: ALPS04751110
    Feature: [Module]Display Driver

--- drivers/misc/mediatek/video/mt6580/videox/mtkfb_debug.c
+++ drivers/misc/mediatek/video/mt6580/videox/mtkfb_debug.c
@@ -75,7 +75,7 @@
 /* Global variable declarations */
 /* --------------------------------------------------------------------------- */
 unsigned int g_enable_uart_log = 0;
-unsigned int g_mobilelog = 1;
+unsigned int g_mobilelog = 0;
 unsigned int g_fencelog = 0; /*Fence Log*/
 #ifdef CONFIG_ARCH_MT6570
 unsigned int g_loglevel = 3; /*DISPMSG level is DEFAULT_LEVEL==3*/
